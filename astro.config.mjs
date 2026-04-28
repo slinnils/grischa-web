097981d01ea1b50f1f14f960dd76ce1e8da253a8
slinnils <imbachnils@hotmail.ch>
2026-04-29 00:14:38 +0200

    minor fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://grischa-web.ch",
+  
   vite: {
     plugins: [tailwindcss()],
   },
